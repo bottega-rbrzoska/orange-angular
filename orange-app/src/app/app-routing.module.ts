3f4d61808598a7cf2215bb0df40b82ce8019bc6e
Rafał Brzoska <rafal@brzoska.net.pl>
2019-06-17 13:27:01 +0200

    Zadanie 2: Components and routing

--- orange-app/src/app/app-routing.module.ts
+++ orange-app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'contact', component: ContactComponent },
+  { path: 'products', component: ProductsListComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- orange-app/src/app/app.module.ts
+++ orange-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    ContactComponent,
+    ProductsListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,

1090a632ca233407d15a6991bce1be95ab6c6a37
Rafał Brzoska <rafal@brzoska.net.pl>
2019-06-17 16:26:11 +0200

    Directives demo

--- orange-app/src/app/app.component.ts
+++ orange-app/src/app/app.component.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  arr = [
+    {id: 1, name: 'jeden'},
+    {id: 2, name: 'dwa'}
+  ]
   title = 'orange-app';
+  isActive = true;
   initialValue = 1;
   testIncrementedHandler(counterValue) {
     alert('Counter: ' + counterValue)
--- orange-app/src/app/test.directive.ts
+++ orange-app/src/app/test.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appTest]'
 })
-export class TestDirective {
+export class TestDirective implements OnInit {
 
-  constructor() { }
 
+  @Input() appTest = 'red';
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    this.renderer.addClass(this.el.nativeElement, 'd-block')
+  }
+
+  ngOnInit() {
+    this.renderer.setStyle(this.el.nativeElement, 'background', this.appTest)
+  }
+
+  @HostListener('mouseover')
+  handleClick() {
+    this.renderer.setStyle(this.el.nativeElement, 'background', 'blue')
+
+  }
 }

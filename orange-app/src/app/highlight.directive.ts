dd54d3c37db9a5b6429158ba7151eba4876139ae
Rafał Brzoska <rafal@brzoska.net.pl>
2019-06-18 09:25:44 +0200

    highlight directive (zadanie 3)

--- orange-app/src/app/highlight.directive.ts
+++ orange-app/src/app/highlight.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  @Input() secondClass;
+  @Input() appHighlight;
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+  }
+  @HostListener('mouseover')
+  handleMouseover() {
+    if(this.appHighlight) {
+      this.renderer.addClass(this.el.nativeElement, this.appHighlight)
+    }
+  }
+  @HostListener('mouseout')
+  handleMouseout() {
+    if(this.appHighlight) {
+      this.renderer.removeClass(this.el.nativeElement, this.appHighlight)
+    }
+
+  }
 
 }
